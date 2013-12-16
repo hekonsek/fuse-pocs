01c8fc1e898244585df39abe6bcca812e48d4893
Henryk Konsek <hekonsek@gmail.com>
2013-12-16 16:37:47 +0100

    Added stream processing to XML normalizer.

--- fuse-poc-camel-xml-normalizer/src/main/java/fuse/pocs/camel/xml/normalizer/XmlElementNormalizationTransformer.java
+++ fuse-poc-camel-xml-normalizer/src/main/java/fuse/pocs/camel/xml/normalizer/XmlElementNormalizationTransformer.java
@@ -1,21 +1,21 @@
 package fuse.pocs.camel.xml.normalizer;
 
-import com.sun.org.apache.xalan.internal.xsltc.trax.TransformerFactoryImpl;
 import org.apache.camel.Body;
 
 import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
-import java.io.StringReader;
+import java.io.InputStream;
 import java.io.StringWriter;
 
 public class XmlElementNormalizationTransformer {
 
-    public String process(@Body String xml) throws Exception {
+    public String process(@Body InputStream xml) throws Exception {
         StringWriter transformedXml = new StringWriter();
-        Source source = new StreamSource(new StringReader(xml));
-        Transformer transformer = new TransformerFactoryImpl().newTransformer();
+        Source source = new StreamSource(xml);
+        Transformer transformer = TransformerFactory.newInstance().newTransformer();
         transformer.transform(source, new StreamResult(transformedXml));
         transformedXml.flush();
         return transformedXml.toString();
