7255e723e0a46162462ede47db34dedbc935284b
Henryk Konsek <hekonsek@gmail.com>
2013-12-27 13:46:09 +0100

    Created blueprint-lists POC.

---
+++ fuse-pocs-blueprint-lists-itest/src/test/java/camelpoc/BlueprintListsTest.java
@@ -0,0 +1,57 @@
+package camelpoc;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+
+import javax.inject.Inject;
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.ops4j.pax.exam.CoreOptions.maven;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.configureConsole;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.karafDistributionConfiguration;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.keepRuntimeFolder;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.logLevel;
+import static org.ops4j.pax.exam.karaf.options.LogLevelOption.LogLevel;
+
+@RunWith(PaxExam.class)
+public class BlueprintListsTest extends Assert {
+
+    @Inject
+    List<String> tokens;
+
+    @Configuration
+    public Option[] configuration() {
+        return new Option[]{
+                karafDistributionConfiguration()
+                        .frameworkUrl(
+                                maven().groupId("org.apache.karaf")
+                                        .artifactId("apache-karaf")
+                                        .type("zip")
+                                        .version("2.3.3"))
+                        .karafVersion("2.3.3")
+                        .name("Apache Karaf")
+                        .unpackDirectory(new File("target/pax"))
+                        .useDeployFolder(false),
+                keepRuntimeFolder(),
+                configureConsole().ignoreLocalConsole().ignoreRemoteShell(),
+
+                logLevel(LogLevel.INFO),
+
+                mavenBundle().groupId("fuse-pocs").artifactId("fuse-poc-blueprint-lists").version("1.0-SNAPSHOT"),
+        };
+    }
+
+    @Test
+    public void shouldInjectSingletonList() {
+        // Then
+        assertEquals(Arrays.asList("foo", "bar"), tokens);
+    }
+
+}
