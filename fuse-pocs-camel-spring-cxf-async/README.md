555505c780872cfe48152e3ae08108bc568910c7
Henryk Konsek <hekonsek@gmail.com>
2014-02-05 09:58:44 +0100

    Refactoring.

--- fuse-pocs-camel-spring-cxf-async/README.md
+++ fuse-pocs-camel-spring-cxf-async/README.md
@@ -1,6 +1,6 @@
 JBoss Fuse proof of concept - Asynchronous processing of REST requests with Camel, CXF and Jetty
 =====
 
-This example demonstrates how to leverage Camel and
+This example demonstrates how to leverage [Camel Asynchronous engine](http://camel.apache.org/async.html) and
 [Jetty continuations](https://wiki.eclipse.org/Jetty/Feature/Continuations)
 to asynchronously process HTTP requests coming to the REST CXF server.
--- fuse-pocs-camel-spring-cxf-async/src/test/java/fuse/pocs/camel/spring/cxf/async/AsynchronousProcessor.java
+++ fuse-pocs-camel-spring-cxf-async/src/test/java/fuse/pocs/camel/spring/cxf/async/AsynchronousProcessor.java
@@ -2,13 +2,21 @@ package fuse.pocs.camel.spring.cxf.async;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
 
 public class AsynchronousProcessor implements Processor {
 
     static final String RESPONSE = "RESPONSE";
 
+    static private final Logger LOG = LoggerFactory.getLogger(AsynchronousProcessor.class);
+
     @Override
     public void process(Exchange exchange) throws Exception {
+        LOG.info("Sleeping...");
+        TimeUnit.SECONDS.sleep(30);
         exchange.getIn().setBody(RESPONSE);
     }
 
